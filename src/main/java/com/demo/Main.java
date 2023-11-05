c73f3bcd0337aa81ea39ed9436bf3f22689c1dad
xin.luo <xin.luo@yaduo.com>
2023-11-06 00:01:43 +0800

    first commit

---
+++ src/main/java/com/demo/Main.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author ${AUTHOR}
+ * @since 2023-11-04 19:21
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/demo/first.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * show demo
+ *
+ * @author 小欣欣
+ * @since 2023-11-04 20:04
+ */
+public class first {
+
+    public void test(){
+        System.out.println("hhhh");
+    }
+}
